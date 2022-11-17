f9441a3de58021ba108c7286e6e72f3b0df410e1
Mendu Kowshik Reddy <mendukowshikreddy@gmail.com>
2022-11-17 21:46:41 +0530

    parent n child communcation

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import  {useState} from 'react'
 function App() {
 
   // state
+ let [alien,setalien]=useState('BigChill')
 
+const car=(data)=>{   //car for beta
+  console.log("Data sent by bacchha is ",data)
+}
 
   return (
     <div className="text-center text-danger">
      <h2>Hey Root here!!</h2>
      {/* nested component... */}
-     <Test1></Test1>
+
+     <Test1  alien={alien} car={car} />     {/* data is sent to child   ... alien={alien} goes as object's key n value pair into props*/}
+
+
     </div>
   );
 }
--- src/components/Test1.js
+++ src/components/Test1.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import Test2 from './Test2'
+import React, { useState } from 'react'
 
-export default function Test1() {
+export default function Test1(props) {  //recievd as object ( x={data}  ==> this comes as an object)
+                      // destructuring use kar sakthe hai props ke badhal--------
+console.log("Data came is ",props.alien)
+
+//  data to be sent to the parent ....dadd will be sending a car.. send in that
+let [age,setage]=useState('3356')
   return (
+
     <div>
       <h2>Test 1</h2>
-      <Test2/>
+      <h4>Well Dada has given me {props.alien}</h4>    {/* parent to child data transfer */}
+      <button className='btn btn-success' onClick={()=>props.car(age)}>Send to pappa</button>
     </div>
   )
 }
