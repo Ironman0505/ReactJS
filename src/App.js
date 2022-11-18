dca70ef19fc7a6236012a621f4b9244600644d15
Mendu Kowshik Reddy <mendukowshikreddy@gmail.com>
2022-11-18 21:10:18 +0530

    Useeffect hook usage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
 import Test1 from './components/Test1'
-import  {useState} from 'react'
+import  {useEffect, useState} from 'react'
 
 function App() {
 
   // state
- let [alien,setalien]=useState('BigChill')
+let [users,setusers]=useState([])
 
-const car=(data)=>{   //car for beta
-  console.log("Data sent by bacchha is ",data)
-}
+let [c1,setc1]=useState(0)
+let [c2,setc2]=useState(0)
 
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/users')
+    .then(res=>res.json())
+    .then(data=>setusers(data))
+  },[c1])   // useeffect gets xecuted with c1 state change..... view the  ntwrk tab
+
+  console.log(users)
   return (
     <div className="text-center text-danger">
-     <h2>Hey Root here!!</h2>
-     {/* nested component... */}
-
-     <Test1  alien={alien} car={car} />     {/* data is sent to child   ... alien={alien} goes as object's key n value pair into props*/}
-
-
+   <h2>Hello Chitti</h2>
+   <h3>{c1}</h3>
+   <button className='btn btn-secondary m-4' onClick={()=>setc1(c1+1)}>Cntr 1</button>
+   <h3>{c2}</h3>
+   <button className='btn btn-danger m-4' onClick={()=>setc2(c2+1)}>Cntr 2</button>
     </div>
   );
 }
